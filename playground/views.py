fb2e43068d00caf01ac9caaf8300cc8a8e1320c4
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2026-04-03 15:09:37 +0200

    feat: try template

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def say_hello(request):
-    return HttpResponse("Hello, world!")
+    return render(request, 'hello.html', {'name': 'Gigi'})
