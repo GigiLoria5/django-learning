6d3b2a930fb07f97a61a7bcaf8379dfd834eda99
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2026-04-07 23:25:02 +0200

    feat: add query in view

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from store.models import Product
+
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Gigi'})
+    query_set = Product.objects.all()
+
+    for product in query_set:
+        print(product.title)
+
+    return render(request, "hello.html", {"name": "Gigi"})
