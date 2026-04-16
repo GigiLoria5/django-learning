6fabab4db1fa1cc54971941cfb8b5163475a9ff3
Luigi Loria <luigi.loria@outlook.com>
2026-04-16 23:25:26 +0200

    feat: add DRF base example

--- djangolearning/urls.py
+++ djangolearning/urls.py
@@ -25,4 +25,5 @@ admin.site.index_title = "Welcome to the Storefront Administration"
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("playground/", include("playground.urls")),
+    path("store/", include("store.urls")),
 ] + debug_toolbar_urls()
--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view()
+def product_list(request: Request) -> Response:
+    return Response("ok")
+
+
+@api_view()
+def product_detail(request: Request, product_id: str) -> Response:
+    return Response(product_id)
