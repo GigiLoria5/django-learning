33d83d4b26ba5249662f8d930ad958ac5e2b860e
Luigi Loria <luigi.loria@outlook.com>
2026-04-18 10:57:43 +0200

    feat: add ModelSerializer for Product and Collection

--- store/serializers.py
+++ store/serializers.py
@@ -2,25 +2,34 @@ from decimal import Decimal
 
 from rest_framework import serializers
 
-from store.models import Product, Collection
+from store.models import Collection, Product
 
 TAX_RATE = Decimal(1.22)
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ["id", "title"]
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(
-        max_digits=6, decimal_places=2, source="unit_price"
-    )
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "title",
+            "unit_price",
+            "price_with_tax",
+            "collection",
+            "collection_link",
+        ]
+
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(), view_name="collection-detail",
+    collection_link = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(),
+        view_name="collection-detail",
+        source="collection",
     )
 
     @staticmethod
