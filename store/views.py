620a90d8bfdf21423df24d1401ea9987e33212f9
Luigi Loria <luigi.loria@outlook.com>
2026-04-18 09:51:09 +0200

    feat: add product serializer and view logic

--- store/views.py
+++ store/views.py
@@ -1,13 +1,23 @@
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.request import Request
 from rest_framework.response import Response
+
+from store.models import Product
+from store.serializers import ProductSerializer
 
 
 @api_view()
 def product_list(request: Request) -> Response:
-    return Response("ok")
+    products_qs = Product.objects.all()
+    serializer = ProductSerializer(products_qs, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request: Request, product_id: str) -> Response:
-    return Response(product_id)
+    product = get_object_or_404(
+        Product, pk=product_id
+    )
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
