c12724e337ba35467a81a61219542f723796ef3b
Luigi Loria <luigi.loria@outlook.com>
2026-04-19 11:52:26 +0200

    feat: add OrderingFilter

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -14,9 +14,10 @@ from store.serializers import CollectionSerializer, ProductSerializer, ReviewSer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
     search_fields = ["title", "description", "collection__title"]
+    ordering_fields = ["unit_price", "inventory", "last_updated"]
 
     def get_serializer_context(self):
         return {"request": self.request}
