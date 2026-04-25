995dfae066c8579db05bebb01bdf243c0a23a171
Luigi Loria <luigi.loria@outlook.com>
2026-04-25 22:11:00 +0200

    fix: accept Bearer token

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,5 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
@@ -8,6 +9,17 @@ class UserCreateSerializer(BaseUserCreateSerializer):
             "username",
             "email",
             "password",
+            "first_name",
+            "last_name",
+        ]
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = [
+            "id",
+            "username",
+            "email",
             "first_name",
             "last_name",
         ]
--- djangolearning/settings.py
+++ djangolearning/settings.py
@@ -138,18 +138,17 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("JWT",),
+    "AUTH_HEADER_TYPES": ("Bearer", "JWT"),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
 }
 
 DJOSER = {
     "TOKEN_MODEL": None,
     "SERIALIZERS": {
         "user_create": "core.serializers.UserCreateSerializer",
+        "current_user": "core.serializers.UserSerializer",
     },
 }
 
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-}
 
 AUTH_USER_MODEL = "core.User"
